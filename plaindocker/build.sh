7935f5fabe3ff5c5c26eafc68e85a4f9944221f8
Itamar Ostricher <itamarost@gmail.com>
2017-04-05 14:48:58 +0300

    super basic greeter service with OpenAPI Python service & gRPC C++ service using plain Docker approach

---
+++ plaindocker/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Building builder images"
+docker build -t ygreet-builder:latest -f builder/Dockerfile.cppgo builder
+docker build -t ygreet-pybuild:latest -f builder/Dockerfile.python builder
+
+echo "Making artifacts"
+cd app && make && cd ..
+
+echo "Building service images"
+docker build -t cppgreet-svc:latest -f app/Dockerfile.cppgreet app
+docker build -t pygreet-svc:latest -f app/Dockerfile.pygreet app
