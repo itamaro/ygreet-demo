d435ca76c8dc347b7af9b7ed5c06db79db522d3c
Itamar Ostricher <itamarost@gmail.com>
2017-04-18 19:17:19 +0300

    trying to use container builder

---
+++ containerbuilder/builder/Dockerfile.cpp
@@ -0,0 +1,17 @@
+FROM ubuntu:16.04
+
+RUN apt-get update && \
+    apt-get install --no-install-recommends -y build-essential autoconf automake curl make g++ unzip libtool ca-certificates pkg-config
+
+COPY grpc/ /tmp/
+
+RUN cd /tmp/grpc/third_party/protobuf && \
+    ./autogen.sh && \
+    ./configure && \
+    make && \
+    make install && \
+    ldconfig /usr/local/lib && \
+    cd ../.. && \
+    make && \
+    make install && \
+    cd / && rm -rf /tmp/grpc
