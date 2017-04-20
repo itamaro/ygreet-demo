ca53b06df0bec171d4ee3fdd8541c9158cd2bc48
Itamar Ostricher <itamarost@gmail.com>
2017-04-20 17:49:01 +0300

    missing update to 1.2.4 in custom installer script

--- withybt/3rdparty/install-grpc-cc.sh
+++ withybt/3rdparty/install-grpc-cc.sh
@@ -2,8 +2,8 @@
 
 set -e
 curl https://codeload.github.com/google/boringssl/tar.gz/78684e5b222645828ca302e56b40b9daff2b2d27 | tar zxf - -C /tmp
-mv /tmp/boringssl-78684e5b222645828ca302e56b40b9daff2b2d27/* grpc-1.2.2/third_party/boringssl
-cd grpc-1.2.2
+mv /tmp/boringssl-78684e5b222645828ca302e56b40b9daff2b2d27/* grpc-1.2.4/third_party/boringssl
+cd grpc-1.2.4
 make
 make install
 ldconfig /usr/local/lib
