de0590c8db03c1ed5a366d6e4d81111a8e3c2091
Itamar Ostricher <itamarost@gmail.com>
2017-04-20 10:55:16 +0300

    working with container builder

---
+++ containerbuilder/app/pygreet_svc.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+import os
+
+import grpc
+
+import service_pb2
+import service_pb2_grpc
+
+
+CPPGREETER_SVC = os.environ.get('CPPGREETER', 'localhost:50051')
+
+
+def say_hello(name=None):
+    channel = grpc.insecure_channel(CPPGREETER_SVC)
+    stub = service_pb2_grpc.GreeterStub(channel)
+    response = stub.SayHello(service_pb2.HelloRequest(name=name))
+    return {
+        'pygreeter': 'Hello {}!'.format(name),
+        'cppgreeter': response.message,
+    }
+
+
+if __name__ == '__main__':
+  print(say_hello('main'))
