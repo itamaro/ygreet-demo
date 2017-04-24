d5e5c854235c109d53d6e436a32ed4c213bfedbb
Itamar Ostricher <itamarost@gmail.com>
2017-04-24 12:41:35 +0300

    split cloudbuild.yaml to one for builder images and one for app images to allow caching the builder images (keep also the unified one for comparison)

--- containerbuilder/build-and-deploy.sh
+++ containerbuilder/build-and-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-gcloud container builds submit --config cloudbuild.yaml .
+gcloud container builds submit --config cloudbuild.unified.yaml .
 PROJECT_ID="$(gcloud config get-value project 2>/dev/null)"
 echo "Deploying app to GKE project $PROJECT_ID using kubectl default cluster"
 sed 's/$PROJECT_ID/'$PROJECT_ID'/g' k8s-app.yaml | kubectl apply -f - --record
